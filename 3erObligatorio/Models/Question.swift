dfa0b6cb01a0b04acf13b19d2874a42a5770fccf
Nicolas <nicolas.handalian@gmail.com>
2019-06-17 20:27:08 -0300

    models

---
+++ 3erObligatorio/Models/Question.swift
@@ -0,0 +1,9 @@
+//
+//  Question.swift
+//  3erObligatorio
+//
+//  Created by Nicolás Handalian on 6/17/19.
+//  Copyright © 2019 Nicolás Handalian. All rights reserved.
+//
+
+import Foundation
---
+++ 3erObligatorio/Models/QuestionCategory.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionCategory.swift
+//  3erObligatorio
+//
+//  Created by Nicolás Handalian on 6/17/19.
+//  Copyright © 2019 Nicolás Handalian. All rights reserved.
+//
+
+import Foundation
