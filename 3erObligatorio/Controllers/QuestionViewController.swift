5c6a25bf0c5e26a12a2040ed26cbcc55dd462f27
Nicolas <nicolas.handalian@gmail.com>
2019-06-27 23:58:27 -0300

    shuffleoptions

--- 3erObligatorio/Controllers/QuestionViewController.swift
+++ 3erObligatorio/Controllers/QuestionViewController.swift
@@ -43,15 +43,53 @@ class QuestionViewController: UIViewController{
     func updateView(){
         questionTextView.text = question!.question
         questionTextView.sizeToFit()
+        let options = shuffleOptions(correct: question!.correct_answer!, incorrects: question!.incorrect_answers!)
         
-        option1Button.setTitle(question!.correct_answer, for:.normal)
+        option1Button.setTitle(options[0], for:.normal)
         
-        option2Button.setTitle(question!.incorrect_answers![0], for:.normal)
+        option2Button.setTitle(options[1], for:.normal)
         
-        option3Button.setTitle(question!.incorrect_answers![1], for:.normal)
+        option3Button.setTitle(options[2], for:.normal)
         
-        option4Button.setTitle(question!.incorrect_answers![2], for:.normal)
+        option4Button.setTitle(options[3], for:.normal)
         
+    }
+    
+    func shuffleOptions(correct:String, incorrects:[String])->[String]{
+        let randomOptions = Int.random(in: 0...3)
+        var op1 = ""
+        var op2 = ""
+        var op3 = ""
+        var op4 = ""
+        switch randomOptions {
+            case 0:
+                op1 = correct + "."
+                op2 = incorrects[0]
+                op3 = incorrects[1]
+                op4 = incorrects[2]
+            case 1:
+                op1 = incorrects[0]
+                op2 = correct + "."
+                op3 = incorrects[1]
+                op4 = incorrects[2]
+            case 2:
+                op1 = incorrects[0]
+                op2 = incorrects[1]
+                op3 = correct + "."
+                op4 = incorrects[2]
+            case 3:
+                op1 = incorrects[0]
+                op2 = incorrects[1]
+                op3 = incorrects[2]
+                op4 = correct + "."
+            default:
+                op1 = incorrects[0]
+                op2 = incorrects[1]
+                op3 = incorrects[2]
+                op4 = correct + "."
+            
+        }
+        return [op1,op2,op3,op4]
     }
     
 }
