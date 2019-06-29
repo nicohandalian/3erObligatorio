cf76dc5c5e5219f9e5935967a3451e129bde7543
Nicolas <nicolas.handalian@gmail.com>
2019-06-28 21:12:10 -0300

    loser alert

--- 3erObligatorio/Controllers/QuestionViewController.swift
+++ 3erObligatorio/Controllers/QuestionViewController.swift
@@ -16,6 +16,8 @@ class QuestionViewController: UIViewController{
     @IBOutlet weak var option2Button: UIButton!
     @IBOutlet weak var option3Button: UIButton!
     @IBOutlet weak var option4Button: UIButton!
+    var correctOption: Int?
+    var options: [String]?
     
     override func viewDidLoad() {
         alterLayout()
@@ -50,15 +52,15 @@ class QuestionViewController: UIViewController{
         questionTextView.text = question!.question
         questionTextView.sizeToFit()
         
-        let options = shuffleOptions(correct: question!.correct_answer!, incorrects: question!.incorrect_answers!)
+        options = shuffleOptions(correct: question!.correct_answer!, incorrects: question!.incorrect_answers!)
         
-        option1Button.setTitle(options[0], for:.normal)
+        option1Button.setTitle(options![0], for:.normal)
         
-        option2Button.setTitle(options[1], for:.normal)
+        option2Button.setTitle(options![1], for:.normal)
         
-        option3Button.setTitle(options[2], for:.normal)
+        option3Button.setTitle(options![2], for:.normal)
         
-        option4Button.setTitle(options[3], for:.normal)
+        option4Button.setTitle(options![3], for:.normal)
         
     }
     
@@ -70,26 +72,31 @@ class QuestionViewController: UIViewController{
         var op4 = ""
         switch randomOptions {
             case 0:
+                correctOption = 1
                 op1 = correct + "."
                 op2 = incorrects[0]
                 op3 = incorrects[1]
                 op4 = incorrects[2]
             case 1:
+                correctOption = 2
                 op1 = incorrects[0]
                 op2 = correct + "."
                 op3 = incorrects[1]
                 op4 = incorrects[2]
             case 2:
+                correctOption = 3
                 op1 = incorrects[0]
                 op2 = incorrects[1]
                 op3 = correct + "."
                 op4 = incorrects[2]
             case 3:
+                correctOption = 4
                 op1 = incorrects[0]
                 op2 = incorrects[1]
                 op3 = incorrects[2]
                 op4 = correct + "."
             default:
+                correctOption = 4
                 op1 = incorrects[0]
                 op2 = incorrects[1]
                 op3 = incorrects[2]
@@ -98,5 +105,53 @@ class QuestionViewController: UIViewController{
         }
         return [op1,op2,op3,op4]
     }
+    
+    func loserAlert(){
+        let loserAlert = UIAlertController(title: "Wrong Answer :(", message: "The correct answer was: \(options![correctOption!])", preferredStyle: .alert)
+        loserAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: self.indexHandler))
+        self.present(loserAlert, animated: true, completion: nil)
+        
+    }
+    
+    func indexHandler(alert: UIAlertAction!){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func option1Selected(_ sender: Any) {
+        if(correctOption! == 1){
+            
+        }
+        else{
+            loserAlert()
+        }
+    }
+    @IBAction func option2Selected(_ sender: Any) {
+        if(correctOption! == 2){
+            
+        }
+        else{
+            loserAlert()
+            
+        }
+    }
+    @IBAction func option3Selected(_ sender: Any) {
+        if(correctOption! == 3){
+            
+        }
+        else{
+            loserAlert()
+            
+        }
+    }
+    @IBAction func option4Selected(_ sender: Any) {
+        if(correctOption! == 4){
+            
+        }
+        else{
+            loserAlert()
+            
+        }
+    }
+    
     
 }
